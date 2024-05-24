af4839d9cffb3a2a28c6c0224df15b4140720527
XingXao <xandrnf@gmail.com>
2024-05-24 13:42:11 +0500

    done

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -4,6 +4,15 @@ public class Main {
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
-        //todo код писать тут
+        String allLettersBig = "АБВГДЖЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        String allLettersSmall = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        for (int i = 0; i < allLettersBig.length(); i++) {
+            System.out.println(allLettersBig.charAt(i) + " - " + (int) allLettersBig.charAt(i));
+        }
+        System.out.println();
+        for (int i = 0; i < allLettersSmall.length(); i++) {
+            System.out.println(allLettersSmall.charAt(i) + " - " + (int) allLettersSmall.charAt(i));
+        }
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+        String input = new Scanner(System.in).nextLine();
+        boolean isValid = true;
+        int spaceCout = 0;
+        final String ALLLATTERS = "АБВГДЖЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ -абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == ' ') {
+                spaceCout++;
+            }
+            if (!ALLLATTERS.contains(String.valueOf(input.charAt(i)))) {
+                isValid = false;
+                break;
+            }
+        }
+        if (isValid && spaceCout == 2) {
+            System.out.println("Фамилия: " + input.substring(0, input.indexOf(' ')) + "\n" +
+                    "Имя: " + input.substring(input.indexOf(' '), input.lastIndexOf(' ')).trim() + "\n" +
+                    "Отчество: " + input.substring(input.lastIndexOf(' ')).trim());
+        } else
+            System.out.println("веденная строка не является ФИО");
+
+
     }
 }
